66c19ae509e0073e51d875cfd27949be683ae5af
mq7253180 <mq7253180@126.com>
2025-03-18 08:08:52 +0800

    xxx

--- auth-server-sharding/src/main/java/com/quincy/auth/service/impl/UserServiceShardingProxyImpl.java
+++ auth-server-sharding/src/main/java/com/quincy/auth/service/impl/UserServiceShardingProxyImpl.java
@@ -16,19 +16,19 @@ import com.quincy.sdk.o.User;
 public class UserServiceShardingProxyImpl extends UserServiceImpl implements UserServiceShardingProxy {
 	@Override
 	@Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED, rollbackFor = Throwable.class)
-	public UserEntity update(@ShardingKey(snowFlake = false)long shardingKey, UserEntity vo) {
+	public UserEntity update(@ShardingKey long shardingKey, UserEntity vo) {
 		return this.update(vo);
 	}
 
 	@Override
 	@ReadOnly
-	public Long findUserId(@ShardingKey(snowFlake = false)long shardingKey, String loginName) {
+	public Long findUserId(@ShardingKey long shardingKey, String loginName) {
 		return this.findUserId(loginName);
 	}
 
 	@Override
 	@ReadOnly
-	public User find(@ShardingKey(snowFlake = false)long shardingKey, Long id, Client client) {
+	public User find(@ShardingKey long shardingKey, Long id, Client client) {
 		User user = this.find(id, client);
 		if(user!=null)
 			user.setShardingKey(shardingKey);
@@ -37,25 +37,25 @@ public class UserServiceShardingProxyImpl extends UserServiceImpl implements Use
 
 	@Override
 	@Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED, rollbackFor = Throwable.class)
-	public void updatePassword(@ShardingKey(snowFlake = false)long shardingKey, Long userId, String password) {
+	public void updatePassword(@ShardingKey long shardingKey, Long userId, String password) {
 		this.updatePassword(userId, password);
 	}
 
 	@Override
 	@Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED, rollbackFor = Throwable.class)
-	public void add(@ShardingKey(snowFlake = false)long shardingKey, UserEntity vo) {
+	public void add(@ShardingKey long shardingKey, UserEntity vo) {
 		this.userRepository.save(vo);
 	}
 
 	@Override
 	@Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED, rollbackFor = Throwable.class)
-	public boolean createMapping(@ShardingKey(snowFlake = false)long shardingKey, String loginName, Long userId) {
+	public boolean createMapping(@ShardingKey long shardingKey, String loginName, Long userId) {
 		return this.createMapping(loginName, userId);
 	}
 
 	@Override
 	@Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED, rollbackFor = Throwable.class)
-	public int deleteMapping(@ShardingKey(snowFlake = false)long shardingKey, String loginName) {
+	public int deleteMapping(@ShardingKey long shardingKey, String loginName) {
 		return this.loginUserMappingRepository.deleteByLoginName(loginName);
 	}
 }
