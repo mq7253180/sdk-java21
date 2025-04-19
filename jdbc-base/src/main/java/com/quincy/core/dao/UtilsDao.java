cbb5447029d18e89ab1fcbfe7d53c0c0f680eeea
mq7253180 <mq7253180@126.com>
2025-04-19 16:06:17 +0800

    xxx

--- jdbc-base/src/main/java/com/quincy/core/dao/UtilsDao.java
+++ jdbc-base/src/main/java/com/quincy/core/dao/UtilsDao.java
@@ -7,7 +7,7 @@ import com.quincy.sdk.annotation.jdbc.JDBCDao;
 
 @JDBCDao
 public interface UtilsDao {
-	@ExecuteQuery(sql = "SELECT AUTO_INCREMENT FROM information_schema.TABLES WHERE table_name=?;", returnItemType = BigInteger.class)
+	@ExecuteQuery(sql = "SELECT AUTO_INCREMENT FROM information_schema.TABLES WHERE table_name=?", returnItemType = BigInteger.class)
 	public BigInteger selectAutoIncreament(String name);
 	@ExecuteQuery(sql = "SELECT LAST_INSERT_ID()", returnItemType = BigInteger.class)
 	public BigInteger selectLastInsertId();
